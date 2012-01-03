f32a652e3e681903079a52ede12bd7ab560d4aac
Sean Zehnder <seanzehnder@gmail.com>
2012-01-03 14:12:12 -0700

    now compiles for iOS as well

---
+++ builds/xcode/ZeroMQ-ios/ZeroMQ-ios/ZeroMQ-ios.h
@@ -0,0 +1,13 @@
+//
+//  ZeroMQ-ios.h
+//  ZeroMQ-ios
+//
+//  Created by Sean Zehnder on 1/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZeroMQ_ios : NSObject
+
+@end
---
+++ builds/xcode/ZeroMQ-ios/ZeroMQ-ios/ZeroMQ-ios.m
@@ -0,0 +1,23 @@
+//
+//  ZeroMQ-ios.m
+//  ZeroMQ-ios
+//
+//  Created by Sean Zehnder on 1/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZeroMQ-ios.h"
+
+@implementation ZeroMQ_ios
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
